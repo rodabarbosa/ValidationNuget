8db47c8e08a38dc6742c45bee0e28a78270c4ed8
Rodrigo Araujo Barbosa <rodrigoarabarbosa@outlook.com>
2023-11-26 20:41:50 -0300

    Incluso compatibilidade com .NET8

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Validação de documentos brasileiros
 
 Os métodos de gerar documentos são disponibilizados somente para auxilio aos desenvolvedores durante o processo de desenvolvimento de suas aplicações.
 
+### Versão 1.3.0
+
+- Inclusão de compatibilidade a .NET 7.0
+
 ### Versão 1.2.1
 
 - Alterado para que dependencia seja uma versão minima e não uma versão fixa.
--- Sirb.Validation.Test/Exceptions/StateNotFoundExceptionTest.cs
+++ Sirb.Validation.Test/Exceptions/StateNotFoundExceptionTest.cs
@@ -4,68 +4,65 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using Xunit;
 
-namespace Sirb.Validation.Test.Exceptions
+namespace Sirb.Validation.Test.Exceptions;
+
+public class StateNotFoundExceptionTest
 {
-    public class StateNotFoundExceptionTest
+    private const string ExpectedMessage = "State not found";
+
+    [Fact]
+    public void NotFoundException_Should_Be_Created()
     {
-        private const string ExpectedMessage = "State not found";
+        var exception = new StateNotFoundException();
+        Assert.NotNull(exception);
+    }
 
-        [Fact]
-        public void NotFoundException_Should_Be_Created()
+    [Fact]
+    public void NotFoundException_Should_Have_Correct_Inner_Exception()
+    {
+        var expectedInnerException = new Exception();
+        var exception = new StateNotFoundException(expectedInnerException);
+
+        Assert.Equal(expectedInnerException, exception.InnerException);
+    }
+
+    [Fact]
+    public void NotFoundException_Should_Have_Correct_Message()
+    {
+        var exception = new StateNotFoundException(ExpectedMessage);
+
+        Assert.Equal(ExpectedMessage, exception.Message);
+    }
+
+    [Theory]
+    [InlineData(true, "Exception message")]
+    [InlineData(false, "Exception message")]
+    [InlineData(true, "")]
+    public void NotFoundException_When_Meets_Condition(bool condition, string message)
+    {
+        if (condition)
         {
-            var exception = new StateNotFoundException();
-            Assert.NotNull(exception);
+            Assert.Throws<StateNotFoundException>(() => StateNotFoundException.ThrowIf(condition, message));
+            return;
         }
 
-        [Fact]
-        public void NotFoundException_Should_Have_Correct_Inner_Exception()
-        {
-            var expectedInnerException = new Exception();
-            var exception = new StateNotFoundException(expectedInnerException);
+        StateNotFoundException.ThrowIf(condition, message);
+        Assert.True(true);
+    }
 
-            Assert.Equal(expectedInnerException, exception.InnerException);
+    [Fact]
+    public void NotFoundException_Serialization()
+    {
+        var e = new StateNotFoundException(ExpectedMessage);
+
+        using (Stream s = new MemoryStream())
+        {
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(s, e);
+            s.Position = 0; // Reset stream position
+            e = (StateNotFoundException)formatter.Deserialize(s);
         }
 
-        [Fact]
-        public void NotFoundException_Should_Have_Correct_Message()
-        {
-            var exception = new StateNotFoundException(ExpectedMessage);
-
-            Assert.Equal(ExpectedMessage, exception.Message);
-        }
-
-        [Theory]
-        [InlineData(true, "Exception message")]
-        [InlineData(false, "Exception message")]
-        [InlineData(true, "")]
-        public void NotFoundException_When_Meets_Condition(bool condition, string message)
-        {
-            if (condition)
-            {
-                Assert.Throws<StateNotFoundException>(() => StateNotFoundException.ThrowIf(condition, message));
-                return;
-            }
-
-            StateNotFoundException.ThrowIf(condition, message);
-            Assert.True(true);
-        }
-
-        [Fact]
-        public void NotFoundException_Serialization()
-        {
-            var e = new StateNotFoundException(ExpectedMessage);
-
-            using (Stream s = new MemoryStream())
-            {
-                var formatter = new BinaryFormatter();
-#pragma warning disable SYSLIB0011
-                formatter.Serialize(s, e);
-                s.Position = 0; // Reset stream position
-                e = (StateNotFoundException)formatter.Deserialize(s);
-#pragma warning restore SYSLIB0011
-            }
-
-            Assert.Equal(ExpectedMessage, e.Message);
-        }
+        Assert.Equal(ExpectedMessage, e.Message);
     }
 }
